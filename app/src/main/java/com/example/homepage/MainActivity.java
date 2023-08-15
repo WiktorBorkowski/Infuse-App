7dbcf730c3438b8c326a7d34086d1c374f0528e1
clodaghobrien1 <141751445+clodaghobrien1@users.noreply.github.com>
2023-08-15 16:35:23 +0100

    progress?

--- app/src/main/java/com/example/homepage/MainActivity.java
+++ app/src/main/java/com/example/homepage/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main);
 
         mtoolbar=findViewById(R.id.toolbar);
         setSupportActionBar(mtoolbar);
